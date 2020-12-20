b4cad668a4f9aa87203635fabdd91ad3f65b4f1f
somaxa8 <silviofrancoxa8@gmail.com>
2020-12-20 18:06:11 -0400

    rename build-image.sh to install.sh

--- gradle/build-image.sh
+++
@@ -1,5 +0,0 @@
-native-image \
-    --no-server \
-    --no-fallback \
-    --allow-incomplete-classpath \
-    -jar build/libs/bootcli.jar
---
+++ gradle/install.sh
@@ -0,0 +1,2 @@
+native-image --no-server --no-fallback --allow-incomplete-classpath -jar build/libs/bootcli.jar
+sudo mv ./bootcli /usr/local/bin/
